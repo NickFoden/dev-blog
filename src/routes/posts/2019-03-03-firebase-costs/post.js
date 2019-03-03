c1f2ba03242570e2a2a9d03f80b378b05c033643
Nick Foden <nickfoden@gmail.com>
2019-03-03 17:22:27 -0500

    Adding a blog post about firebase outgoing bandwidth usage and component did unmount

---
+++ src/routes/posts/2019-03-03-firebase-costs/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Firing a function on tab/browser closer`,
+  tags: ["react", "firebase", "costs", "nickfoden"],
+  spoiler: "A story about Firebase actually costing money. . I know right",
+  getContent: () => import("./document.mdx")
+};
