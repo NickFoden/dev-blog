39707fa38eadc7259b5a0cd20a789f14bf8d8eb2
Nick Foden <nickfoden@gmail.com>
2019-03-03 17:36:59 -0500

    Fixing spelling error

--- src/routes/posts/2019-03-03-firebase-costs/post.js
+++ src/routes/posts/2019-03-03-firebase-costs/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `Firing a function on tab/browser closer`,
+  title: `Firing a function on tab/browser close`,
   tags: ["react", "firebase", "costs", "nickfoden"],
   spoiler: "A story about Firebase actually costing money. . I know right",
   getContent: () => import("./document.mdx")
