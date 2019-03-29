4ccf18564eb8fccf629ac4c25bd40e4ded7ab89d
Nick Foden <nickfoden@gmail.com>
2019-03-29 11:51:49 -0400

    New post on robot mode needs styling wip

---
+++ src/routes/posts/2019-03-29-roboto-mode/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `🤖 Roboto Mode `,
+  tags: ["react", "navi", "sanity", "health", "robot"],
+  spoiler: "Closing the laptop and throwing away the key . . . values.",
+  getContent: () => import("./document.mdx")
+};
