45f43e89cd584c7ae526a491e2bd4df09ced0617
Nick Foden <nickfoden@gmail.com>
2019-05-07 15:13:31 -0400

    Adding initial chantastic post

---
+++ src/routes/posts/2019-05-07-hot-garbage/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `🔥 Hot Garbage 🗑`,
+  tags: ["ReactRally", "Michael Chan"],
+  spoiler: "Michael Chan - Hot Garbage : Clean Code is Dead",
+  getContent: () => import("./document.mdx")
+};
