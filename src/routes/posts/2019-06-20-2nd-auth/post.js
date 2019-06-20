973b3f428aedc0f76cc3b5455eced222b049ae39
Nick Foden <nickfoden@gmail.com>
2019-06-20 11:22:34 -0400

    Adding new post about firebase logging you out on new user creation

---
+++ src/routes/posts/2019-06-20-2nd-auth/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: `Getting logged out creating new firebase users`,
+  tags: ["firebase", "cms", "auth"],
+  spoiler:
+    "Setting up an Admin Dashboard for a Firebase project? Create a new user while you stay logged in.",
+  getContent: () => import("./document.mdx")
+};
